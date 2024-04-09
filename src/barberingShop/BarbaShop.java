0944000a73526fd00b71b4fd34132ed8e8c40edb
Akwasititus <www.ayamgatitus@gmail.com>
2024-04-09 07:51:40 +0000

    created the 'waitingqueue,currentcustomer,and the chair count variable'

--- src/barberingShop/BarbaShop.java
+++ src/barberingShop/BarbaShop.java
@@ -1,4 +1,11 @@
 package barberingShop;
 
+import java.util.LinkedList;
+
 public class BarbaShop {
+
+    final LinkedList<String> waitingQueue = new LinkedList<>();
+    String currentCustomer = null;
+
+    private static final int CHAIR_COUNT = 5;
 }
