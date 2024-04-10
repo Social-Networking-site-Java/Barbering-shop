590efb0642dbb91367c0cf3df2d1361aeee91811
Akwasititus <www.ayamgatitus@gmail.com>
2024-04-10 08:26:50 +0000

    added the 'saveCustomer and addVIPCustomer function'

--- src/barberingShop/BarbaShop.java
+++ src/barberingShop/BarbaShop.java
@@ -38,7 +38,7 @@ public class BarbaShop {
                 addVIPCustomer();
                 break;
             default:
-                addOrdinaryCustomer(event);
+                //addOrdinaryCustomer(event);
         }
     }
 
